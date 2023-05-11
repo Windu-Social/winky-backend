a24dc07128a6cb962223d91db060a26e40dff38b
Nguyen Hoang Lam <nguyenhoanglam@Nguyens-MacBook-Pro.local>
2023-05-11 22:37:57 +0700

    fix: 🐛 ci/cd firebase hosting

--- src/messages/messages.gateway.ts
+++ src/messages/messages.gateway.ts
@@ -9,7 +9,6 @@ import { MessagesService } from './messages.service';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
 import { Server, Socket } from 'socket.io';
-import { messagesErrors } from './errors/index.errors';
 import { FindAllMessagesDto } from './dto/find-message.dto';
 
 @WebSocketGateway(8080, { namespace: 'messages', cors: true })
